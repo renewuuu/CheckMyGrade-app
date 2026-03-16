824bc852d78542e7f9924b9821ea1e782e7f7260
Rene Wu <a0906735802@gmail.com>
2026-03-15 22:32:54 -0700

    Create CheckMyGrade.py

---
+++ CheckMyGrade.py
@@ -0,0 +1,32 @@
+class Student:
+    def __init__(self, email, first_name, last_name, course_id, grade, marks):
+
+        self.email = email
+        self.first_name = first_name
+        self.last_name = last_name
+        self.course_id = course_id
+        self.grade = grade
+        self.marks = marks
+
+    def display_record(self):
+
+        print(
+            self.email,
+            self.first_name,
+            self.last_name,
+            self.course_id,
+            self.grade,
+            self.marks
+        )
+
+
+class StudentManager:
+    def __init__(self):
+        self.students = []
+
+    def add_student(self, student):
+        self.students.append(student)
+
+    def display_students(self):
+        for s in self.students:
+            s.display_record()
